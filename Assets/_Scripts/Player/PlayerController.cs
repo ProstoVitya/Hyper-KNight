d0404a3f979fdf53864de4c23f26c18236bacd74
rarxn <79763776+rarxn@users.noreply.github.com>
2023-03-14 03:42:07 +0400

    na4alo
    2 spella+test

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -5,19 +5,35 @@ using Utilities.Observer;
 
 namespace Player
 {
+
+    [RequireComponent(typeof(CharacterController))]
     [RequireComponent(typeof(AudioSource))]
     public class PlayerController : Subject
     {
+<<<<<<< Updated upstream
         private SimpleSpell[] _spells;
 
         private Transform _transform;
+=======
+        //временно SerializeField
+        [SerializeField] private SimpleSpell[] _spells;
+        [SerializeField] private Transform _castPoint;
+        [SerializeField] private float _speed = 6.0f; // скорость персонажа
+        [SerializeField] private float _rotationSpeed = 200.0f; // скорость персонажа
+>>>>>>> Stashed changes
         private AudioSource _audioSource;
+        private CharacterController _controller;
+        
+
 
         private void Awake()
         {
-            _transform = transform;
             _audioSource = transform.GetComponent<AudioSource>();
+<<<<<<< Updated upstream
 
+=======
+            _controller = GetComponent<CharacterController>();
+>>>>>>> Stashed changes
             //todo: подгружать выбранные в меню способности
         }
 
@@ -29,11 +45,19 @@ namespace Player
         {
             var spell = _spells[index];
 
-            if (spell.TimeToNextAttack <= 0)
+            if (spell.TimeToNextUse <= 0)
             {
-                spell.Use(_transform);
+                spell.Use(_castPoint);
                 spell.UseEffects(_audioSource);
             }
+        }
+        private void Update()
+        {
+            float vertical = Input.GetAxis("Vertical");
+            float rotation = Input.GetAxis("Horizontal") * _rotationSpeed * Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+            Vector3 move = transform.forward * vertical * _speed;
+            _controller.Move(move * Time.deltaTime);
         }
     }
 }
